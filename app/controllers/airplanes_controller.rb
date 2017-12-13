89ec8ed63245db250513e08ec0a21b4ac4a2271c
chriswillphoto <christophernekich@gmail.com>
2017-12-13 13:05:37 +1100

    reversed the order

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -4,7 +4,7 @@ class AirplanesController < ApplicationController
   # GET /airplanes
   # GET /airplanes.json
   def index
-    @airplanes = Airplane.all
+    @airplanes = Airplane.all.reverse
   end
 
   # GET /airplanes/1
